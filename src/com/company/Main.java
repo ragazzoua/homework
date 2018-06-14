d65083f2413a80841d81f83ac989953d3b3e7200
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-14 18:35:56 +0300

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("First Item");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.println("Your Name " + name);
     }
 }

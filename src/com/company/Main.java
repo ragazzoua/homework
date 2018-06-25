175b2e99dc14c256f27b4683613b3c07e5df13cf
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-25 17:04:23 +0300

    find biggest value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Give me too numbers and press Enter after each");
 
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        System.out.println("First number " + x);
+        System.out.println("Second number " + y);
+        System.out.println("-----------------");
+        if (x > y) {
+            System.out.println("Max Value " + x);
+            System.out.println("Min Value " + y);
+        }
+        else {
+            System.out.println("Min Value " + x);
+            System.out.println("Max Value " + y);
+        }
     }
+
 }

3dc1771eb5a4d614f1a65d50f3439c6b6c32ffca
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-25 15:36:17 +0300

    Task2_1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = args.length - 1; i > 0; i--) {
-            System.out.println(args[i]);
-        }
+
 
     }
 }

817bc198e77bdb1d6ac9d0e9379f129e8d7e5370
Serhii A. Miroshnychenko <s.a.miroshnychenko@globallogic.com>
2018-06-26 17:51:01 +0300

    task 2_2 finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,21 @@ public class Main {
 
 
         Arrays.sort(values);
+        System.out.println(Arrays.toString(values));
+        System.out.println("Min to Max");
+        System.out.println("---------------------------");
+        for (int i = 0; i < values.length; i++) {
+            System.out.println("Value " + values[i]);
+        }
+        System.out.println("---------------------------");
+        System.out.println("Max to Min");
+        for (int i = values.length - 1; i > 0; i--) {
+            System.out.println("Value " + values[i]);
+        }
 
     }
 
-    static void findMinMax(int[] values){
+    static void findMinMax(int[] values) {
 
         int min = values[0];
         int max = values[0];
